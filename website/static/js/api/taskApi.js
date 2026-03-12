1e1939c0a4ff5e996f1d0352b27d2f29f1141957
Вячеслав <62754486+vbelyavcev@users.noreply.github.com>
2026-03-12 18:53:32 +0300

    сделал отображение задач с пагинацией

--- website/static/js/api/taskApi.js
+++ website/static/js/api/taskApi.js
@@ -1,3 +1,4 @@
+// js/api/taskApi.js
 import { API_CONFIG, ENDPOINTS } from './config.js';
 
 export const taskApi = {
@@ -32,6 +33,44 @@ export const taskApi = {
             return await response.json();
         } catch (error) {
             console.error('Add task failed:', error);
+            throw error;
+        }
+    },
+
+    async getTasks(page = 1, pageSize = 10) {
+        try {
+
+            const url = `${API_CONFIG.baseUrl}${ENDPOINTS.TASK.GET_TASKS(page, pageSize)}`;
+            console.log('Request URL:', url); // Для отладки
+
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: API_CONFIG.headers
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP ${response.status}`);
+            }
+            return await response.json();
+        } catch (error) {
+            console.error('Get tasks failed:', error);
+            throw error;
+        }
+    },
+
+    async getTaskDetail(taskId) {
+        try {
+            const response = await fetch(`${API_CONFIG.baseUrl}${ENDPOINTS.TASK.GET_TASK_DETAIL(taskId)}`, {
+                method: 'GET',
+                headers: API_CONFIG.headers
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP ${response.status}`);
+            }
+            return await response.json();
+        } catch (error) {
+            console.error(`Get task detail failed for ID ${taskId}:`, error);
             throw error;
         }
     }
